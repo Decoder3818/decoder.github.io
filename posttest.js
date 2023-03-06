ebe227fb46b626e9f6c6a064fb886d6dd89dfd42
Decoder3818 <104771589+Decoder3818@users.noreply.github.com>
2023-03-06 11:52:46 +0100

    Update posttest.js

--- posttest.js
+++ posttest.js
@@ -12,6 +12,7 @@ var ajaxPromisify = (url, type, data, headers) => {
                 Cache-Control: no-cache,
                 User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3},
                 Connection: keep-alive
+                     }
       // xhrFields: {
       //   withCredentials: true
       // },

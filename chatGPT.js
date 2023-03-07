8cdf4e673f3d1e026d925df7ada7ae959a4e8204
Decoder3818 <104771589+Decoder3818@users.noreply.github.com>
2023-03-07 10:01:10 +0100

    Update chatGPT.js

--- chatGPT.js
+++ chatGPT.js
@@ -1,51 +1,52 @@
-var ajaxCall = (Key, Url , Prompt) => {
+var ajaxCall = (Key, Url, Prompt) => {
   return new Promise((resolve, reject) => {
     $.ajax({
-  Url,
-  type: "POST",
-  dataType: "json",
-  data: JSON.stringify({
-    model: "text-davinci-002",
-    prompt: Prompt,
-    max_tokens: 1024,
-    n: 1,
-    temperature: 0.5,
-  }),
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${Key}`,
-  },
-    crossDomain: true,
-      success: function (response, status, xhr) {
-        resolve({ response, status, xhr })
+      url: Url,
+      type: "POST",
+      dataType: "json",
+      data: JSON.stringify({
+        model: "text-davinci-002",
+        prompt: Prompt,
+        max_tokens: 1024,
+        n: 1,
+        temperature: 0.5,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Key}`,
       },
-      error: function (response, status, xhr) {
-        const err = new Error('xhr error')
-        err.status = xhr.status
-        reject(err)
-      }
-})
-  })
-}
+      crossDomain: true,
+      success: function (response, status, xhr) {
+        resolve({ response, status, xhr });
+      },
+      error: function (xhr, status, error) {
+        const err = new Error('xhr error');
+        err.status = xhr.status;
+        reject(err);
+      },
+    });
+  });
+};
 
-const url = 'https://api.openai.com/v1/';
+const url = "https://api.openai.com/v1/completions";
 
 (function () {
-  const template = document.createElement('template')
+  const template = document.createElement("template");
   template.innerHTML = `
       <style>
       </style>
       <div id="root" style="width: 100%; height: 100%;">
       </div>
-    `
+    `;
   class MainWebComponent extends HTMLElement {
-
-    async function post(apiKey,endpoint, prompt) {
-  const { response } = await ajaxcall(apiKey, `${url}${endpoint}`, prompt)
-    return response;
-  };
-
+    async post(apiKey, endpoint, prompt) {
+      const { response } = await ajaxCall(
+        apiKey,
+        `${url}/${endpoint}`,
+        prompt
+      );
+      return response;
+    }
   }
-
-  customElements.define('custom-widget', MainWebComponent)
-})()
+  customElements.define("custom-widget", MainWebComponent);
+})();

60c82d0c2fe8bb3f14de529a45b8808add949d8c
Decoder3818 <104771589+Decoder3818@users.noreply.github.com>
2024-01-11 08:54:21 +0100

    Create app.js
    test of integration logic

---
+++ app.js
@@ -0,0 +1,54 @@
+var ajaxCall = (key, url, messages) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: url,
+      type: "POST",
+      dataType: "json",
+      data: JSON.stringify({
+        model: "gpt-4-1106-preview",
+        messages: messages,
+        max_tokens: 1024,
+        n: 1,
+        temperature: 0.5,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${key}`,
+      },
+      crossDomain: true,
+      success: function (response, status, xhr) {
+        resolve({ response, status, xhr });
+      },
+      error: function (xhr, status, error) {
+        const err = new Error('xhr error');
+        err.status = xhr.status;
+        reject(err);
+      },
+    });
+  });
+};
+
+const url = "https://api.openai.com/v1";
+
+(function () {
+  const template = document.createElement("template");
+  template.innerHTML = `
+      <style>
+      </style>
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `;
+  class MainWebComponent extends HTMLElement {
+    async post(apiKey, endpoint, messages) {
+      messages = JSON.parse(messages);
+      const { response } = await ajaxCall(
+        apiKey,
+        `${url}/${endpoint}`,
+        messages
+      );
+      console.log(response.choices[0].message.content);
+      return response.choices[0].message.content;
+    }
+  }
+  customElements.define("custom-widget", MainWebComponent);
+})();
